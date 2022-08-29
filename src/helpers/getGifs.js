0a1af7a819df6cccf9a4a44020e426f2cae3b4ce
Alexis Gilberth Renteria <gilberthr10@gmail.com>
2022-08-29 14:31:06 -0500

    DELETE: API_KEY const

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 const API_KEY = '';
 export const getGifs = async (category) => {
 
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APY_KEY}&q=${category}&limit=5`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=5`;
   const result = await fetch(url);
   const { data } = await result.json();
 
